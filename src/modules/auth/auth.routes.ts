7ae204b842180ad6e5bd1df8709dcd0b99a6662d
Samekshya <samikshyabaniya003@gmail.com>
2026-01-02 00:21:08 +0545

    auth routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { register } from "./auth.controller";
 
 const router = Router();
 
+router.post("/register", register);
+
 router.get("/test", (_req, res) => {
-  return res.json({ success: true, message: "Auth routes working" });
+  res.json({ success: true, message: "Auth routes working" });
 });
 
 export default router;
