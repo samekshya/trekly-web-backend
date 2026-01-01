07d3d8e42562e0af751c6564bc417202958f12d0
Samekshya <samikshyabaniya003@gmail.com>
2026-01-01 23:27:45 +0545

    env

---
+++ src/config/env.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const env = {
+  PORT: process.env.PORT || "5050",
+  MONGO_URI: process.env.MONGO_URI || "",
+  JWT_SECRET: process.env.JWT_SECRET || "",
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "7d",
+};
+
+if (!env.MONGO_URI) {
+  throw new Error("MONGO_URI is missing in .env");
+}
+
+if (!env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is missing in .env");
+}
