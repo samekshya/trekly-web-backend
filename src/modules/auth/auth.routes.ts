d925d9f2984275f105774afe8c1cf1f95556a13c
Samekshya <samikshyabaniya003@gmail.com>
2026-01-02 00:37:12 +0545

    routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { register } from "./auth.controller";
+import { validateDto } from "../../middlewares/validateDto";
+import { loginSchema } from "./dto/login.dto";
+import { registerSchema } from "./dto/register.dto";
+import { loginUser, registerUser } from "./auth.controller";
 
 const router = Router();
 
@@ -7,6 +10,7 @@ router.get("/test", (_req, res) => {
   res.json({ success: true, message: "Auth routes working" });
 });
 
-router.post("/register", register);
+router.post("/register", validateDto(registerSchema), registerUser);
+router.post("/login", validateDto(loginSchema), loginUser);
 
 export default router;
