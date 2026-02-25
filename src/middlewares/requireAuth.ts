ae6821e748fa00be2d491476fc8fa669de8859c9
Samekshya <samikshyabaniya003@gmail.com>
2026-02-25 20:09:35 +0545

    update requireAuth to check cookies and bearer token

--- src/middlewares/requireAuth.ts
+++ src/middlewares/requireAuth.ts
@@ -3,16 +3,30 @@ import jwt from "jsonwebtoken";
 
 export const requireAuth = (req: any, res: Response, next: NextFunction) => {
   try {
-    const authHeader = req.headers.authorization;
+    //const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({
-        success: false,
-        message: "No token provided",
-      });
-    }
+    //if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      //return res.status(401).json({
+        //success: false,
+        //message: "No token provided",
+     // });
+    //}
 
-    const token = authHeader.split(" ")[1];
+    //const token = authHeader.split(" ")[1];
+    // 1. Try to get token from cookie first
+let token = req.cookies?.token;
+
+// 2. If no cookie, fallback to Authorization header
+if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+  token = req.headers.authorization.split(' ')[1];
+}
+
+// 3. If still no token, return an error
+if (!token) {
+  return res.status(401).json({ message: 'Not authorized, no token provided' });
+}
+
+
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
 
