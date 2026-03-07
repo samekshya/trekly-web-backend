c88b41380090a1b4e07e3bdc7208cac205618fb3
Samekshya <samikshyabaniya003@gmail.com>
2026-03-07 11:27:23 +0545

    tests passed

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,21 +1,17 @@
 import nodemailer from "nodemailer";
 import { env } from "../config/env";
 
-export const sendEmail = async (
-  to: string,
-  subject: string,
-  html: string
-) => {
+export const sendEmail = async (to: string, subject: string, html: string) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: env.EMAIL_USER,
+      pass: env.EMAIL_PASS,
     },
   });
 
   const info = await transporter.sendMail({
-    from: process.env.EMAIL_FROM,
+    from: env.EMAIL_FROM,
     to,
     subject,
     html,
@@ -23,3 +19,29 @@ export const sendEmail = async (
 
   console.log("Email sent:", info.messageId);
 };
+// import nodemailer from "nodemailer";
+
+// export const sendEmail = async (to: string, subject: string, html: string) => {
+//   console.log("EMAIL_USER:", process.env.EMAIL_USER);
+//   console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+//   console.log("EMAIL_FROM:", process.env.EMAIL_FROM);
+
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const info = await transporter.sendMail({
+//     from: process.env.EMAIL_FROM,
+//     to,
+//     subject,
+//     html,
+//   });
+
+//   console.log("Email sent:", info.messageId);
+// };
